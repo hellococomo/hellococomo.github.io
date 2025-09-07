c96233bd626c05d0332da55280b219cfc4a73eb3
Hayley <hwarren21@bloomberg.net>
2025-09-07 21:38:26 +0100

    v4

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["robots.txt"]),
 	mimeTypes: {".txt":"text/plain"},
 	_: {
-		client: {start:"_app/immutable/entry/start.DPHDE5KF.js",app:"_app/immutable/entry/app.k9xboXHt.js",imports:["_app/immutable/entry/start.DPHDE5KF.js","_app/immutable/chunks/Dl6x-zE_.js","_app/immutable/chunks/Bt-Wkm9A.js","_app/immutable/chunks/BZ5a9Li2.js","_app/immutable/entry/app.k9xboXHt.js","_app/immutable/chunks/BZ5a9Li2.js","_app/immutable/chunks/Bt-Wkm9A.js","_app/immutable/chunks/DsnmJJEf.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.MV6X7UrS.js",app:"_app/immutable/entry/app.D4-vUhIE.js",imports:["_app/immutable/entry/start.MV6X7UrS.js","_app/immutable/chunks/B26lY8Un.js","_app/immutable/chunks/Bt-Wkm9A.js","_app/immutable/chunks/BZ5a9Li2.js","_app/immutable/entry/app.D4-vUhIE.js","_app/immutable/chunks/BZ5a9Li2.js","_app/immutable/chunks/Bt-Wkm9A.js","_app/immutable/chunks/DsnmJJEf.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js'))
